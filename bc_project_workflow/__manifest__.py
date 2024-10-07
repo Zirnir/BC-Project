f2cc560eebcbd10d4522940e6a76150251b0906b
Zirnir <mauricedecourtray@gmail.com>
2024-10-07 10:08:54 +0200

    first commit

---
+++ bc_project_workflow/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'BC_Project_Workflow',
+    'depends': ['project'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'application': True,
+}
---
+++ bc_project_workflow/models/project_task.py
@@ -0,0 +1,47 @@
+from odoo import models, api
+from odoo.exceptions import UserError
+class Task(models.Model):
+    _inherit = "project.task"
+
+    @api.model
+    def create(self, values):
+        task = super(Task, self).create(values)
+        return task
+
+    def write(self, values):
+        if 'stage_id' in values:
+            stage = self.env['project.task.type'].browse(values['stage_id'])
+
+            if stage.name == "En cours":
+                for user in self.user_ids:
+                    tasks_in_progress = self.env['project.task'].search([
+                        ('user_ids', 'in', [user.id]),
+                        ('stage_id.name', '=', 'En cours'),
+                        ('id', '!=', self.id)
+                    ])
+                    if tasks_in_progress:
+                        raise UserError(f"L'utilisateur {user.name} a déjà une tâche en cours.") 
+        if self.stage_id.name == "En cours" or ('stage_id' in values and stage.name == "En cours"):
+            if 'user_ids' in values:
+                user_ids_operations = values.get('user_ids', [])
+                new_user_ids = []
+                
+                for operation in user_ids_operations:
+                    if operation[0] == 6:
+                        new_user_ids = operation[2]
+                    elif operation[0] == 4:
+                        new_user_ids.append(operation[1])
+
+                if new_user_ids:
+                    users = self.env['res.users'].browse(new_user_ids)
+                    for user in users:
+                        tasks_in_progress = self.env['project.task'].search([
+                            ('user_ids', 'in', [user.id]),
+                            ('stage_id.name', '=', 'En cours'),
+                            ('id', '!=', self.id)
+                        ])
+                        if tasks_in_progress:
+                            raise UserError(f"L'utilisateur {user.name} a déjà une tâche en cours.")
+        
+        result = super(Task, self).write(values)
+        return result
