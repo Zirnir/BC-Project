d9fa9b863bd4036081ce2120f55429b05c195f42
Zirnir <98953738+Zirnir@users.noreply.github.com>
2024-10-09 16:23:29 +0200

    Update project_task.py
    clean code

--- bc_project_workflow/models/project_task.py
+++ bc_project_workflow/models/project_task.py
@@ -1,12 +1,8 @@
 from odoo import models, api
 from odoo.exceptions import UserError
+
 class Task(models.Model):
     _inherit = "project.task"
-
-    @api.model
-    def create(self, values):
-        task = super(Task, self).create(values)
-        return task
 
     def write(self, values):
         if 'stage_id' in values:
@@ -21,6 +17,7 @@ class Task(models.Model):
                     ])
                     if tasks_in_progress:
                         raise UserError(f"L'utilisateur {user.name} a déjà une tâche en cours.") 
+        
         if self.stage_id.name == "En cours" or ('stage_id' in values and stage.name == "En cours"):
             if 'user_ids' in values:
                 user_ids_operations = values.get('user_ids', [])
@@ -42,6 +39,5 @@ class Task(models.Model):
                         ])
                         if tasks_in_progress:
                             raise UserError(f"L'utilisateur {user.name} a déjà une tâche en cours.")
-        
-        result = super(Task, self).write(values)
-        return result
+
+        return super(Task, self).write(values)
